01833496a914259a91a8e79220bacbf524539bf0
alexandre.demeure@univ-grenoble-alpes.fr <alexandre.demeure@univ-grenoble-alpes.fr>
2026-03-30 11:18:57 +0200

    fin cours 09

--- src/app/pages/page-03/child-compo/child-compo.ts
+++ src/app/pages/page-03/child-compo/child-compo.ts
@@ -1,5 +1,6 @@
-import { Component, model } from '@angular/core';
+import { Component, effect, model } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-child-compo',
@@ -9,12 +10,19 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
 })
 export class ChildCompo {
   public readonly es = model.required<string>();
-  protected readonly fc = new FormControl<string>('');
+  protected readonly fc = new FormControl<string>('', {nonNullable: true});
 
   constructor() {
     // es -> fc
+    effect(() => {
+      const value = this.es();
+      console.log("es -> fc :", value);
+      this.fc.setValue(value, {emitEvent: false});
+    })
 
     // fc -> es
-    
+    this.fc.valueChanges.pipe(
+      tap( v => console.log("fc -> es : ", v) )
+    ).subscribe(v => this.es.set(v));
   }
 }
--- src/app/pages/page-04/page-04.ts
+++ src/app/pages/page-04/page-04.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { combineLatest, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-page-04',
@@ -8,3 +9,38 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Page04 {}
+
+
+/** 
+ * Constantes vitales
+ */
+interface CV {freq: number, tension: number, temp: number}
+interface people {
+  freq$   : Observable<number> // Fréquence cardiaque
+  tension$: Observable<number> // Tension artériel
+  temp$   : Observable<number> // Température
+}
+declare function cvOK(cv: CV): boolean;
+declare function getPeopleFromName(n: string): people;
+
+function getCvFor(n: string): Observable<CV & { name: string, ok: boolean }> {
+  const p = getPeopleFromName(n);
+  const cv$: Observable<CV> = combineLatest([
+    p.freq$,
+    p.temp$,
+    p.tension$
+  ]).pipe(
+    map(
+      ([freq, temp, tension]) => ({ freq, temp, tension })
+    )
+  );
+
+  return cv$.pipe(
+    map( cv => ({
+        ...cv,
+        name: n,
+        ok: cvOK(cv)
+      })
+    )
+  )
+}
